9f2b1607359290259f9da1a90162a85204c90903
Jonas Freres <jonas.freres@pergroep.net>
2019-02-15 12:44:44 +0100

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,10 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("hello_manon") { req in
+        return "Hello, Manon !\n je voulais te montrer un truc trop cool, mais ca marche pas"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
